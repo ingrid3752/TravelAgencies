e9fc5c548245a3765d690010a698677d36bc5585
ingrid3752 <golj6622@naver.com>
2024-09-09 17:40:52 +0900

    [수정] js

--- Project1/src/main/resources/static/js/accomInsertReservation.js
+++ Project1/src/main/resources/static/js/accomInsertReservation.js
@@ -1,6 +1,6 @@
 function updateId() {
-	var accomCode = document.getElementById("accomCode").value;
-	var memCode = document.getElementById("memCode").value;
+	var accomCode = document.getElementById("accomCode").value ="1";
+	var memCode = document.getElementById("memCode").value = "1";
 	if (accomCode === "1") {
 		accomIdField.value = "1";
 	}
